22b24692aa101e414c16f881e4665d0e5f118caf
Bemhreth <bemhretgezahegn@gmail.com>
2021-01-21 15:03:02 +0300

    reverted

--- lib/screen/input_page.dart
+++ lib/screen/input_page.dart
@@ -15,24 +15,24 @@ class InputPage extends StatefulWidget {
 
 class _InputPageState extends State<InputPage> {
 
-List<String> images=[
-  'asset/1.jpg',
-  'asset/2.jpg',
-  'asset/3.jpg',
-  'asset/4.jpg',
-  'asset/5.jpg',
-  'asset/6.jpg',
-  'asset/7.jpg',
-  'asset/8.jpg',
-  'asset/ምድረ.jpg',
-  'asset/እንሰት.jpg',
-  'asset/እንሰት1.jpg',
-  'asset/እንሾሽላ1.jpg',
-  'asset/እንሾሽላ2.jpg',
-  'asset/ገበጣ.jpg',
-  'asset/ጉራጌ.jpg',
-  'asset/ጉራጌ1.jpg',
-];
+  List<String> images=[
+    'asset/1.jpg',
+    'asset/2.jpg',
+    'asset/3.jpg',
+    'asset/4.jpg',
+    'asset/5.jpg',
+    'asset/6.jpg',
+    'asset/7.jpg',
+    'asset/8.jpg',
+    'asset/ምድረ.jpg',
+    'asset/እንሰት.jpg',
+    'asset/እንሰት1.jpg',
+    'asset/እንሾሽላ1.jpg',
+    'asset/እንሾሽላ2.jpg',
+    'asset/ገበጣ.jpg',
+    'asset/ጉራጌ.jpg',
+    'asset/ጉራጌ1.jpg',
+  ];
   @override
   Widget build(BuildContext context) {
     //csvtolist();
@@ -76,7 +76,7 @@ List<String> images=[
                   ),
                   body: TabBarView(
                     children: [
-                      ResultsPage('ኣመረተ','English','Amharic','Kistanigna'),
+                      ResultsPage('ኣመረተ','English','Amharic','Kistanigna',null),
                       Tab2('ኣመረተ','English','Amharic','Kistanigna'),
                       Tab3(),
                     ],
