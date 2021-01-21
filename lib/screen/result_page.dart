7037f77d6fe427b90b19a32b439f96019d5539ce
Bemhreth <bemhretgezahegn@gmail.com>
2021-01-21 15:02:23 +0300

    reverted

--- lib/screen/result_page.dart
+++ lib/screen/result_page.dart
@@ -3,9 +3,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:dictionary_app/utility/dbconnection.dart';
 import 'package:dictionary_app/utility/dictionarymodel.dart';
-import 'package:sqflite/sqflite.dart';
-
-
 
 
 List<String> savedWords = List<String>();
@@ -16,11 +13,12 @@ class Item {
   final Icon icon;
 }
 class ResultsPage extends StatefulWidget {
-   String definition;
-   String Mainlanguage;
-   String language1;
-   String language2;
-  ResultsPage(this.definition,this.language1,this.language2,this.Mainlanguage);
+  String definition;
+  String Mainlanguage;
+  String language1;
+  String language2;
+  String T;
+  ResultsPage(this.definition,this.language1,this.language2,this.Mainlanguage,this.T);
   @override
 
   _ResultsPageState createState() => _ResultsPageState();
@@ -30,8 +28,8 @@ class _ResultsPageState extends State<ResultsPage> {
 
   Item selectedUser;
   List<Item> users = <Item>[
-    const Item('Kistanigna',Icon(Icons.language,color: Colors.greenAccent,)),
-    const Item('Amharic',Icon(Icons.language,color:  Colors.greenAccent,)),
+    const Item('ክስታንኛ',Icon(Icons.language,color: Colors.greenAccent,)),
+    const Item('አማርኛ',Icon(Icons.language,color:  Colors.greenAccent,)),
     const Item('English',Icon(Icons.language,color:  Colors.greenAccent,)),
   ];
   TextEditingController _controller = TextEditingController();
@@ -40,24 +38,42 @@ class _ResultsPageState extends State<ResultsPage> {
   Stream _stream;
   List<Map> all=List<Map>();
   List<Map> alls=List<Map>();
+  List<Map> allz=List<Map>();
   List<Map<String, dynamic>> all1=List<Map<String, dynamic>>();
   Timer _debounce;
   Color fevcolor=Colors.black;
   Future _futurevalue;
 
   void _search (text)  {
-        if(text.isNotEmpty) {
-      List<Map<String, dynamic>> dummyListData = List<Map<String, dynamic>>();
-    final item= alls.where((e) => e['Kistanigna'] == text || e['Amharic'] == text
-          || e['English']==text);
-    item.forEach((item) {
-      print(item['Kistanigna']);
-      dummyListData.add(item);
-    });
+    if(text.isNotEmpty) {
+      print(text);
+      List<Map> dListData = List<Map>();
+      allz.forEach((element) {
+        print('kinda');
+        switch (widget.Mainlanguage) {
+          case 'English':
+            print('yes');
+            element['English'].toString().toLowerCase();
+            if( element['English'].contains(text.toLowerCase())){
+              dListData.add(element);
+            }
+            break;
+          case 'Amharic':
+            if( element['Amharic'].contains(text)){
+              dListData.add(element);
+            }
+            break;
+          case 'Kistanigna':
+            if( element['Kistanigna'].contains(text)){
+              dListData.add(element);
+            }
+            break;
+        }
+      });
       setState(()  {
-
         all.clear();
-        all.addAll(dummyListData);
+        all.addAll(dListData);
+        dListData.clear();
       });
       return;
     } else {
@@ -67,12 +83,6 @@ class _ResultsPageState extends State<ResultsPage> {
       });
     }
   }
-
-//    _streamController.add("waiting");
-//    Response response = await get(_url + _controller.text.trim(), headers: {"Authorization": "Token " + _token});
-//    _streamController.add(json.decode(response.body));
-//  }
-
   @override
   Future<void> initState()  {
     super.initState();
@@ -84,7 +94,8 @@ class _ResultsPageState extends State<ResultsPage> {
 
   getdictionary() async{
     alls= List.of(await DBprovider.db.getdictionary());
-
+    all= List.of(await DBprovider.db.getdictionary());
+    allz= List.of(await DBprovider.db.getdictionary());
     setState(() {
       all=alls;
 //      print(all);
@@ -93,8 +104,6 @@ class _ResultsPageState extends State<ResultsPage> {
 
   @override
   Widget build(BuildContext context) {
-
-    String text1;
     return Scaffold(
       appBar: AppBar(
         bottom: PreferredSize(
@@ -108,21 +117,35 @@ class _ResultsPageState extends State<ResultsPage> {
                   margin: const EdgeInsets.only(left: 12.0, bottom: 8.0),
                   decoration: BoxDecoration(
                     color: Colors.white,
-                    borderRadius: BorderRadius.circular(24.0),
+//                    borderRadius: BorderRadius.circular(24.0),
                   ),
                   child: TextFormField(
                     cursorColor: Colors.black26,
                     style:TextStyle(color: Colors.black),
-                    onChanged: (String text) {
-                      text1=text;
-                      if (_debounce?.isActive ?? false) _debounce.cancel();
-                      _debounce = Timer(const Duration(milliseconds: 1000), () {
-                        _search(text);
+                    onTap: () {
+                      FocusScopeNode currentFocus = FocusScope.of(context);
+
+                      if (!currentFocus.hasPrimaryFocus) {
+                        currentFocus.unfocus();
+                      }
+                    },
+                    onFieldSubmitted:(String text){
+                      _search(text);
+                      setState(() {
+                        widget.T=text;
                       });
                     },
-                    controller: _controller,
+                    onChanged: (String text) {
+                      if (_debounce?.isActive ?? false) _debounce.cancel();
+                      _debounce = Timer(const Duration(milliseconds: 1000), () {
+                        setState(() {
+                          _search(text);
+                          widget.T=text;
+                        });
+                      });
+                    },
                     decoration: InputDecoration(
-                      hintText: "Search for a word",
+                      hintText: (widget.Mainlanguage=='English')?"Search in English": (widget.Mainlanguage=='Amharic')?"በአማርኛ ይፈልጉ":"በክስታንኛ ሻ",
                       hintStyle: TextStyle(color: Colors.black),
                       contentPadding: const EdgeInsets.only(left: 24.0),
                       border: InputBorder.none,
@@ -136,36 +159,43 @@ class _ResultsPageState extends State<ResultsPage> {
                   color: Colors.white,
                 ),
                 onPressed: () {
-                  _search(text1);
+                  _search(widget.T);
                 },
               ),
               Card(
                 elevation: 5,
                 color: Colors.white,
-                child: DropdownButton<Item>(
-                  value: selectedUser==null?selectedUser=users[0]:selectedUser,
-                  onChanged: (Item Value) {
-                    setState(() {
-                      selectedUser = Value;
-                      widget.Mainlanguage=selectedUser.name;
-                      print(selectedUser.name);
-                    });
-                  },
-                  items: users.map((Item user) {
-                    return  DropdownMenuItem<Item>(
-                      value: user,
-                      child: Row(
-                        children: <Widget>[
-                          user.icon,
-                          SizedBox(width: 10,),
-                          Text(
-                            user.name,
-                            style:  TextStyle(color: Colors.black),
+                child: Row(
+                  children: <Widget>[
+                    DropdownButton<Item>(
+                      dropdownColor: Colors.white,
+                      value: selectedUser==null?selectedUser=users[0]:selectedUser,
+                      onChanged: (Item Value) {
+                        setState(() {
+                          selectedUser = Value;
+                          (selectedUser.name=='ክስታንኛ')?widget.Mainlanguage='Kistanigna':(selectedUser.name=='አማርኛ')?widget.Mainlanguage='Amharic':widget.Mainlanguage=selectedUser.name;
+                          print(selectedUser.name);
+                        });
+                      },
+                      items: users.map((Item user) {
+                        return  DropdownMenuItem<Item>(
+                          value: user,
+                          child: Row(
+                            children: <Widget>[
+                              SizedBox(width: 15,),
+                              user.icon,
+                              SizedBox(width: 10,),
+                              Text(
+                                user.name,
+                                style:  TextStyle(color: Colors.black),
+                              ),
+                            ],
                           ),
-                        ],
-                      ),
-                    );
-                  }).toList(),
+                        );
+                      }).toList(),
+                    ),
+                    Icon(Icons.arrow_drop_down,color: Colors.black,)
+                  ],
                 ),
               ),
             ],
@@ -173,98 +203,96 @@ class _ResultsPageState extends State<ResultsPage> {
         ),
       ),
       body: Container(
-        margin: const EdgeInsets.all(8.0),
-        child: FutureBuilder(
-        future: _futurevalue,
-        builder: (context, snapshot) {
+          margin: const EdgeInsets.all(8.0),
+          child: FutureBuilder(
+              future: _futurevalue,
+              builder: (context, snapshot) {
 
-          switch(snapshot.connectionState){
-            case ConnectionState.none:
-              return Center(child: Text('Waiting...'));
-            case ConnectionState.active:
-              return  Center(child: Text('Waiting...'));
-            case ConnectionState.done:
-              return ListView.builder(
-                itemCount: all.length,
+                switch(snapshot.connectionState){
+                  case ConnectionState.none:
+                    return Center(child: Text('Waiting...'));
+                  case ConnectionState.active:
+                    return  Center(child: Text('Waiting...'));
+                  case ConnectionState.done:
+                    return ListView.builder(
+                      itemCount: all.length,
 
-                itemBuilder: (BuildContext context, int index) {
-                  String word=all[index]['Kistanigna'];
-                  bool isSaved = savedWords.contains(word);
-                  return ListBody(
-                    children: <Widget>[
-                      Card(
-                        elevation: 5,
-                        color: Colors.white,
-                        child: Row(
+                      itemBuilder: (BuildContext context, int index) {
+                        String word=all[index]['Kistanigna'];
+                        bool isSaved = savedWords.contains(word);
+                        return ListBody(
                           children: <Widget>[
-                            Expanded(
-                              child: ListTile(
+                            Card(
+                                elevation: 5,
+                                color: Colors.white,
+                                child: Row(
+                                  children: <Widget>[
+                                    Expanded(
+                                      child: ListTile(
 
-                                title: Text(
-                                  widget.Mainlanguage=='Kistanigna'?  all[index]['Kistanigna']: all[index][widget.Mainlanguage] + "(" + all[index]['Kistanigna'] +
-                                      ")", style: TextStyle(color: Colors.black),),
-                                onTap: () {
-                                  showDialog(
-                                    context: context,
-                                    builder: (BuildContext context) => _buildAboutDialog(context),
-                                  );
-                                  setState(() {
-                                    widget.language1=all[index]['Amharic'];
-                                    widget.language2=all[index]['English'];
-                                    widget.definition=all[index]['Definition'];
-                                  });
-                                },
-                              ),
-                            ),
-                            IconButton(
+                                        title: Text(
+                                          widget.Mainlanguage=='Kistanigna'?  all[index]['Kistanigna']: all[index][widget.Mainlanguage] + "(" + all[index]['Kistanigna'] +
+                                              ")", style: TextStyle(color: Colors.black),),
+                                        onTap: () {
+                                          showDialog(
+                                            context: context,
+                                            builder: (BuildContext context) => _buildAboutDialog(context),
+                                          );
+                                          setState(() {
+                                            widget.language1=all[index]['Amharic'];
+                                            widget.language2=all[index]['English'];
+                                            widget.definition=all[index]['Definition'];
+                                          });
+                                        },
+                                      ),
+                                    ),
+                                    IconButton(
 
-                            icon: Icon(
-                              all[index]['Favorite']!="0"? Icons.star : Icons.star_border,
-                              color: all[index]['Favorite']!="0" ? Colors.red : Colors.black,
-                            ),
-                       onPressed: (){
+                                      icon: Icon(
+                                        all[index]['Favorite']!="0"? Icons.star : Icons.star_border,
+                                        color: all[index]['Favorite']!="0" ? Colors.red : Colors.black,
+                                      ),
+                                      onPressed: (){
 
 //                           print(word);
 
-                           if (all[index]['Favorite']!="0") {
-                             setState(() {
-                               //savedWords.remove(word);
-                               DBprovider.db.updatefev(all[index]['Kistanigna'],0);
-                               getdictionary();
-                               DBprovider.db.deletefavorite(all[index]['Kistanigna']);
+                                        if (all[index]['Favorite']!="0") {
+                                          setState(() {
+                                            //savedWords.remove(word);
+                                            DBprovider.db.updateall(all[index]['id'], Dictionary(Amharic: all[index]['Amharic'],Kistanigna: all[index]['Kistanigna'],English: all[index]['English'],Definition: all[index]['Definition'],Favorite: "0"));
+                                            DBprovider.db.deletefavorite(all[index]['Kistanigna']);
+                                            getdictionary();
+                                          });
+                                        } else {
+                                          setState(() {
+                                            // savedWords.add(word);
+                                            var newinfo= Dictionary(Amharic: all[index]['Amharic'],Kistanigna: all[index]['Kistanigna'],English: all[index]['English'],Definition: all[index]['Definition'],Favorite: "1");
+                                            DBprovider.db.newsfavorite(newinfo);
+                                            DBprovider.db.updateall(all[index]['id'], Dictionary(Amharic: all[index]['Amharic'],Kistanigna: all[index]['Kistanigna'],English: all[index]['English'],Definition: all[index]['Definition'],Favorite: "1"));
+                                            getdictionary();
+                                          });
 
-                           });
-                           } else {
-                             setState(() {
-                              // savedWords.add(word);
-                               DBprovider.db.updatefev(all[index]['Kistanigna'],1);
-                               getdictionary();
-                               var newinfo= Dictionary(Amharic: all[index]['Amharic'],Kistanigna: all[index]['Kistanigna'],English: all[index]['English'],Definition: all[index]['Definition'],Favorite: "1");
-                               DBprovider.db.newsfavorite(newinfo);
-
-                             });
-
-                           }
+                                        }
 
 
-                                    },
-                          )
-                        ],
-                        )
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.all(3.0),
-                      )
-                    ],
-                  );
-                },
-              );
-            default:
-              return Center(child: Text("Loading...",style: TextStyle(fontSize: 40),));
-          }
+                                      },
+                                    )
+                                  ],
+                                )
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.all(3.0),
+                            )
+                          ],
+                        );
+                      },
+                    );
+                  default:
+                    return Center(child: Text("Loading...",style: TextStyle(fontSize: 40),));
+                }
 
-        }
-        )
+              }
+          )
       ),
     );
   }
@@ -296,10 +324,10 @@ class _ResultsPageState extends State<ResultsPage> {
         text: widget.language1+'\n\n',
         style: const TextStyle(color: Colors.white),
         children: <TextSpan>[
-           TextSpan(text:widget.language2+'\n\n\n',style: TextStyle(color: Colors.white)),
+          TextSpan(text:widget.language2+'\n\n\n',style: TextStyle(color: Colors.white)),
 
           TextSpan(
-            text:widget.definition,style: TextStyle(color: Colors.white)
+              text:widget.definition,style: TextStyle(color: Colors.white)
           ),
 
           const TextSpan(text: '.'),
